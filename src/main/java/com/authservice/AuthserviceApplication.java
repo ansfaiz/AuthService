ec44a318cdb26352c69d01e69e48925204ea35e6
md faiyaz ansari <154304294+ansfaiz@users.noreply.github.com>
2025-10-05 03:12:35 +0530

    Update AuthserviceApplication.java

--- src/main/java/com/authservice/AuthserviceApplication.java
+++ src/main/java/com/authservice/AuthserviceApplication.java
@@ -1,4 +1,11 @@
 package com.authservice;
+/*
+ * Copyright 2025 md faiyaz Ansari
+ *
+ *
+ * NOTE: This file has been significantly modified from its original source 
+ * by md faiyaz Ansari on [2025].
+ */
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
